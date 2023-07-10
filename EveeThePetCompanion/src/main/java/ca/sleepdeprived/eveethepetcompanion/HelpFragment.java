013d3d033e2f0cd0a61c2ea30205becd7ff5c61d
UbayAbdulaziz7353 <n01437353@humbermail.ca>
2023-07-10 04:07:58 -0400

    Created contact us button functionality

--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/HelpFragment.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/HelpFragment.java
@@ -6,20 +6,37 @@
 */
 package ca.sleepdeprived.eveethepetcompanion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HelpFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_help, container,false);
+        View view = inflater.inflate(R.layout.fragment_help, container, false);
+        Button contactUsButton = view.findViewById(R.id.contact_us_button);
+        contactUsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+        return view;
+    }
 
+    public void sendEmail() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse(getString(R.string.mailto_chloeissleeping_gmail_com)));
+        startActivity(Intent.createChooser(intent, "Send Email"));
     }
 }
