6446bae84eef90a8deb3d4236d47efa5d7d862be
WeiWenChai7321 <105020438+WeiWenChai7321@users.noreply.github.com>
2023-07-10 02:28:03 -0400

    Implemented sign in with google button - not fully functioning

--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/LoginActivity.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/LoginActivity.java
@@ -9,20 +9,39 @@ package ca.sleepdeprived.eveethepetcompanion;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 9001;
 
     private EditText usernameEditText;
     private EditText passwordEditText;
     private Button loginButton;
+    private SignInButton googleSignInButton;
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +51,13 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.username_edittext);
         passwordEditText = findViewById(R.id.password_edittext);
         loginButton = findViewById(R.id.login_button);
+        googleSignInButton = findViewById(R.id.google_sign_in_button); // Add this line
 
+        // Configure Google Sign-In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,21 +73,62 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+            }
+        });
+
+        try {
+            googleSignInClient.silentSignIn().addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()) {
+                    // Silent sign-in successful, handle the signed-in account
+                    GoogleSignInAccount account = task.getResult();
+                    handleGoogleSignInSuccess(account);
+                } else {
+                    // Silent sign-in failed, handle the error
+                    Exception exception = task.getException();
+                    if (exception instanceof ApiException) {
+                        ApiException apiException = (ApiException) exception;
+                        int statusCode = apiException.getStatusCode();
+                        // Handle the specific error status code
+                        Toast.makeText(this, "Google Sign-In failed: " + statusCode, Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Handle other exceptions
+                        Toast.makeText(this, "Google Sign-In failed: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            // Handle any exceptions
+            Toast.makeText(this, "Google Sign-In failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
+
 
     private boolean performLogin(String username, String password) {
-        boolean isLoggedIn = username.equals(getString(R.string.admin)) && password.equals(getString(R.string.loginpassword));
-
-        if (isLoggedIn) {
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean(getString(R.string.isloggedin), true);
-            editor.apply();
+        if (username.isEmpty() || password.isEmpty()) {
+            // Display a Toast message for empty fields
+            Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+            return false;
         }
 
-        return isLoggedIn;
-    }
+        if (!username.equals(getString(R.string.admin)) || !password.equals(getString(R.string.loginpassword))) {
+            // Display a Toast message for incorrect username/password
+            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
 
+        // Set the isLoggedIn flag in SharedPreferences
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(getString(R.string.isloggedin), true);
+        editor.apply();
+
+        return true;
+    }
 
     private void navigateToMainActivity() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
@@ -70,4 +136,46 @@ public class LoginActivity extends AppCompatActivity {
         finish(); // Finish LoginActivity to prevent going back to it
     }
 
+    private void signInWithGoogle() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleGoogleSignInResult(task);
+        }
+    }
+
+    private void handleGoogleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            // Signed in successfully, navigate to MainActivity or perform additional actions
+            // with the account information
+            handleGoogleSignInSuccess(account);
+        } catch (ApiException e) {
+            // Google Sign-In failed, update UI accordingly
+            handleGoogleSignInFailure(e);
+        }
+    }
+
+    private void handleGoogleSignInSuccess(GoogleSignInAccount account) {
+        // Perform any necessary actions after a successful Google Sign-In
+        // For example, you could authenticate the user on your backend server
+        String email = account.getEmail();
+        Toast.makeText(this, "Signed in with Google: " + email, Toast.LENGTH_SHORT).show();
+
+        navigateToMainActivity();
+    }
+
+    private void handleGoogleSignInFailure(ApiException e) {
+        // Google Sign-In failed, display an error message or handle the failure
+        // You can use the exception's status code to determine the specific error
+        Toast.makeText(this, "Google Sign-In failed: " + e.getStatusCode(), Toast.LENGTH_SHORT).show();
+    }
 }
