b6ff68e0f7d0887cf383e54a2b72cf38a3935e1a
UbayAbdulaziz7353 <n01437353@humbermail.ca>
2023-07-10 04:23:12 -0400

    Changed buttons to ImageButtons, display toast when treat button is pressed (1 functionality for this screen)

--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/StreamFragment.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/StreamFragment.java
@@ -11,25 +11,48 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class StreamFragment extends Fragment {
 
-    private Button btnObstacleAvoidance;
-    private Button btnLineFollowing;
-    private Button btnTreat;
+    private ImageButton btnObstacleAvoidance;
+    private ImageButton btnLineFollowing;
+    private ImageButton btnTreat;
+    private ImageButton btnRecord;
+    private ImageButton btnPicture;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stream, container, false);
 
-        View streamView = view.findViewById(R.id.stream_view);
-        View arrowKeysView = view.findViewById(R.id.arrow_keys_view);
+        btnObstacleAvoidance = view.findViewById(R.id.btn_obstacle_avoidance);
+        btnLineFollowing = view.findViewById(R.id.btn_line_following);
+        btnTreat = view.findViewById(R.id.btn_treat);
+        btnRecord = view.findViewById(R.id.btn_record);
+        btnPicture = view.findViewById(R.id.btn_picture);
+
+        btnObstacleAvoidance.setColorFilter(ContextCompat.getColor(requireContext(), android.R.color.white));
+        btnLineFollowing.setColorFilter(ContextCompat.getColor(requireContext(), android.R.color.white));
+        btnTreat.setColorFilter(ContextCompat.getColor(requireContext(), android.R.color.white));
+        btnRecord.setColorFilter(ContextCompat.getColor(requireContext(), android.R.color.white));
+        btnPicture.setColorFilter(ContextCompat.getColor(requireContext(), android.R.color.white));
+
+        btnTreat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Treat Dispensed", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
 }
+
