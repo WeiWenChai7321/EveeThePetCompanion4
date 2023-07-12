b1066122f18e6c84b8b2c35c0983cf53ffdf27f1
JenniferNguyen5464 <n01435464@humbermail.ca>
2023-07-12 01:59:51 -0400

    Implement add more reminders feature

--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/DashboardFragment.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/DashboardFragment.java
@@ -10,87 +10,93 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class DashboardFragment extends Fragment {
-    private Handler handler;
-    private Runnable removeCrossedOutRemindersRunnable;
-    private SharedPreferences sharedPreferences;
-    private Set<String> crossedOutReminders;
 
-    private CheckBox vetVisitsReminderCheckBox;
+    private List<CheckBox> reminderCheckboxes;
+    private SharedPreferences sharedPreferences;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        handler = new Handler();
-        removeCrossedOutRemindersRunnable = this::removeCrossedOutReminders;
+        LinearLayout remindersLayout = view.findViewById(R.id.reminders_card);
+        EditText editReminderEditText = view.findViewById(R.id.edit_text_reminder);
         sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE);
-        crossedOutReminders = sharedPreferences.getStringSet("crossed_out_reminders", new HashSet<>());
+        reminderCheckboxes = new ArrayList<>();
 
-        vetVisitsReminderCheckBox = view.findViewById(R.id.vet_visits_reminder);
-        vetVisitsReminderCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                crossedOutReminders.add(buttonView.getTag().toString());
-            } else {
-                crossedOutReminders.remove(buttonView.getTag().toString());
-            }
-        });
-
-        // Hide crossed-out reminders
-        if (crossedOutReminders.contains(vetVisitsReminderCheckBox.getTag().toString())) {
-            vetVisitsReminderCheckBox.setVisibility(View.GONE);
+        // Retrieve saved reminders
+        Set<String> savedReminders = sharedPreferences.getStringSet("reminders", new HashSet<>());
+        for (String reminder : savedReminders) {
+            addReminder(remindersLayout, reminder);
         }
+
+        // Add reminder when pressing enter on the keyboard
+        editReminderEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                String reminderText = editReminderEditText.getText().toString().trim();
+                if (!TextUtils.isEmpty(reminderText)) {
+                    addReminder(remindersLayout, reminderText);
+                    editReminderEditText.setText("");
+                    return true;
+                }
+            }
+            return false;
+        });
 
         return view;
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        // Start the handler to remove crossed-out reminders after 5 seconds
-        handler.postDelayed(removeCrossedOutRemindersRunnable, 5000);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        // Stop the handler when the fragment is paused or destroyed
-        handler.removeCallbacks(removeCrossedOutRemindersRunnable);
-    }
-
-    @Override
     public void onDestroyView() {
         super.onDestroyView();
-        // Save the crossed-out reminders in SharedPreferences
+        // Save the reminders in SharedPreferences
+        Set<String> reminders = new HashSet<>();
+        for (CheckBox checkBox : reminderCheckboxes) {
+            if (checkBox.isChecked()) {
+                reminders.add(checkBox.getText().toString());
+            }
+        }
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putStringSet("crossed_out_reminders", crossedOutReminders);
+        editor.putStringSet("reminders", reminders);
         editor.apply();
     }
 
-    private void removeCrossedOutReminders() {
-        ViewGroup remindersLayout = getView().findViewById(R.id.reminders_card);
-        int childCount = remindersLayout.getChildCount();
-        for (int i = childCount - 1; i >= 0; i--) {
-            View childView = remindersLayout.getChildAt(i);
-            if (childView instanceof CheckBox) {
-                CheckBox checkBox = (CheckBox) childView;
-                if (checkBox.isChecked()) {
-                    remindersLayout.removeViewAt(i);
-                    crossedOutReminders.add(checkBox.getTag().toString());
-                }
+    private void addReminder(ViewGroup remindersLayout, String reminderText) {
+        CheckBox reminderCheckBox = new CheckBox(requireContext());
+        reminderCheckBox.setText(reminderText);
+        reminderCheckBox.setChecked(false);
+        reminderCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                removeReminderDelayed((CheckBox) buttonView, remindersLayout);
             }
-        }
+        });
+        remindersLayout.addView(reminderCheckBox);
+        reminderCheckboxes.add(reminderCheckBox);
+    }
+
+    private void removeReminderDelayed(CheckBox checkBox, ViewGroup remindersLayout) {
+        new Handler().postDelayed(() -> {
+            remindersLayout.removeView(checkBox);
+            reminderCheckboxes.remove(checkBox);
+        }, 5000);
     }
 }
