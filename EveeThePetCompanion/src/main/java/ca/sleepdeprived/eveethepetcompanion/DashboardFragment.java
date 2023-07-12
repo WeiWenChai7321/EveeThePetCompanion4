ff70fa42f044a9b4257d74440f0ebff99c3bdcd7
JenniferNguyen5464 <n01435464@humbermail.ca>
2023-07-12 01:33:22 -0400

    Implement reminder feature - crossing off items

--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/DashboardFragment.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/DashboardFragment.java
@@ -6,21 +6,91 @@
 */
 package ca.sleepdeprived.eveethepetcompanion;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DashboardFragment extends Fragment {
+    private Handler handler;
+    private Runnable removeCrossedOutRemindersRunnable;
+    private SharedPreferences sharedPreferences;
+    private Set<String> crossedOutReminders;
+
+    private CheckBox vetVisitsReminderCheckBox;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container,false);
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        handler = new Handler();
+        removeCrossedOutRemindersRunnable = this::removeCrossedOutReminders;
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE);
+        crossedOutReminders = sharedPreferences.getStringSet("crossed_out_reminders", new HashSet<>());
 
+        vetVisitsReminderCheckBox = view.findViewById(R.id.vet_visits_reminder);
+        vetVisitsReminderCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                crossedOutReminders.add(buttonView.getTag().toString());
+            } else {
+                crossedOutReminders.remove(buttonView.getTag().toString());
+            }
+        });
+
+        // Hide crossed-out reminders
+        if (crossedOutReminders.contains(vetVisitsReminderCheckBox.getTag().toString())) {
+            vetVisitsReminderCheckBox.setVisibility(View.GONE);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Start the handler to remove crossed-out reminders after 5 seconds
+        handler.postDelayed(removeCrossedOutRemindersRunnable, 5000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop the handler when the fragment is paused or destroyed
+        handler.removeCallbacks(removeCrossedOutRemindersRunnable);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Save the crossed-out reminders in SharedPreferences
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putStringSet("crossed_out_reminders", crossedOutReminders);
+        editor.apply();
+    }
+
+    private void removeCrossedOutReminders() {
+        ViewGroup remindersLayout = getView().findViewById(R.id.reminders_card);
+        int childCount = remindersLayout.getChildCount();
+        for (int i = childCount - 1; i >= 0; i--) {
+            View childView = remindersLayout.getChildAt(i);
+            if (childView instanceof CheckBox) {
+                CheckBox checkBox = (CheckBox) childView;
+                if (checkBox.isChecked()) {
+                    remindersLayout.removeViewAt(i);
+                    crossedOutReminders.add(checkBox.getTag().toString());
+                }
+            }
+        }
     }
 }
--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/PetProfileFragment.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/PetProfileFragment.java
@@ -6,20 +6,106 @@
 */
 package ca.sleepdeprived.eveethepetcompanion;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PetProfileFragment extends Fragment {
+    private TextView vetVisitsReminderText;
+    private CheckBox vetShotsReminderCheckBox;
+    private CheckBox vetVisitsReminderCheckBox;
+    private SharedPreferences sharedPreferences;
+    private Set<String> crossedOutReminders;
+    private Handler handler;
+    private Runnable removeCrossedOutRemindersRunnable;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_petprofile, container,false);
+        View view = inflater.inflate(R.layout.fragment_petprofile, container, false);
+        vetVisitsReminderText = view.findViewById(R.id.vet_visits_reminder);
+        vetShotsReminderCheckBox = view.findViewById(R.id.vet_shots_reminder);
+        vetVisitsReminderCheckBox = view.findViewById(R.id.vet_visits_reminder);
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE);
+        crossedOutReminders = sharedPreferences.getStringSet("crossed_out_reminders", new HashSet<>());
 
+        // Hide crossed-out reminders
+        if (crossedOutReminders.contains(vetShotsReminderCheckBox.getTag().toString())) {
+            vetShotsReminderCheckBox.setVisibility(View.GONE);
+        }
+        if (crossedOutReminders.contains(vetVisitsReminderCheckBox.getTag().toString())) {
+            vetVisitsReminderCheckBox.setVisibility(View.GONE);
+        }
+
+        handler = new Handler();
+        removeCrossedOutRemindersRunnable = this::removeCrossedOutReminders;
+
+        vetShotsReminderCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                crossedOutReminders.add(buttonView.getTag().toString());
+                buttonView.setVisibility(View.GONE); // Hide the checkbox when checked
+                handler.postDelayed(removeCrossedOutRemindersRunnable, 5000); // Start the handler to remove the reminder after 5 seconds
+            } else {
+                crossedOutReminders.remove(buttonView.getTag().toString());
+            }
+        });
+
+        vetVisitsReminderCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                crossedOutReminders.add(buttonView.getTag().toString());
+                buttonView.setVisibility(View.GONE); // Hide the checkbox when checked
+                handler.postDelayed(removeCrossedOutRemindersRunnable, 5000); // Start the handler to remove the reminder after 5 seconds
+            } else {
+                crossedOutReminders.remove(buttonView.getTag().toString());
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Start the handler to remove crossed-out reminders after 5 seconds
+        handler.postDelayed(removeCrossedOutRemindersRunnable, 5000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop the handler when the fragment is paused or destroyed
+        handler.removeCallbacks(removeCrossedOutRemindersRunnable);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Save the crossed-out reminders in SharedPreferences
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putStringSet("crossed_out_reminders", crossedOutReminders);
+        editor.apply();
+    }
+
+    private void removeCrossedOutReminders() {
+        if (vetShotsReminderCheckBox.getVisibility() == View.GONE) {
+            crossedOutReminders.remove(vetShotsReminderCheckBox.getTag().toString());
+        }
+        if (vetVisitsReminderCheckBox.getVisibility() == View.GONE) {
+            crossedOutReminders.remove(vetVisitsReminderCheckBox.getTag().toString());
+        }
     }
 }
