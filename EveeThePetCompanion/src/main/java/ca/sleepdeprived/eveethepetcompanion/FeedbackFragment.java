5cbb9d6d13b57e7a8af8db18d5663c5467ffa8ea
WeiWenChai7321 <n01447321@humber.ca>
2023-07-15 14:10:22 -0400

    validate feedback fields, send feedback to db

--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/FeedbackFragment.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/FeedbackFragment.java
@@ -1,20 +1,118 @@
+/*Section: 0NA
+  Wei Wen Chai, N01447321
+  John Aquino, N01303112
+  Jennifer Nguyen, N01435464
+  Ubay Abdulaziz, N01437353
+*/
+
 package ca.sleepdeprived.eveethepetcompanion;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class FeedbackFragment extends Fragment {
+
+    private EditText editName;
+    private EditText editPhone;
+    private EditText editEmail;
+    private EditText editComment;
+    private RatingBar ratingBar;
+
+    private FirebaseFirestore db;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_feedback, container,false);
+        View view = inflater.inflate(R.layout.fragment_feedback, container, false);
 
+        // Initialize the views
+        editName = view.findViewById(R.id.edit_name);
+        editPhone = view.findViewById(R.id.edit_phone);
+        editEmail = view.findViewById(R.id.edit_email);
+        editComment = view.findViewById(R.id.edit_comment);
+        ratingBar = view.findViewById(R.id.rating_bar);
+
+        // Initialize Firebase Firestore
+        db = FirebaseFirestore.getInstance();
+
+        // Set up the "Submit Feedback" button click listener
+        Button btnSubmitFeedback = view.findViewById(R.id.btn_submit_feedback);
+        btnSubmitFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the user input
+                String name = editName.getText().toString();
+                String phone = editPhone.getText().toString();
+                String email = editEmail.getText().toString();
+                String comment = editComment.getText().toString();
+                float rating = ratingBar.getRating();
+
+                // Validate phone number
+                if (!isValidPhoneNumber(phone)) {
+                    editPhone.setError("Invalid phone number");
+                    return;
+                }
+
+                // Validate email
+                if (!isValidEmail(email)) {
+                    editEmail.setError("Invalid email address");
+                    return;
+                }
+
+                // Get the device model programmatically
+                String deviceModel = android.os.Build.MODEL;
+
+                // Create a new feedback document with the captured data
+                Feedback feedback = new Feedback(name, phone, email, comment, rating, deviceModel);
+
+                // Save the feedback to Firestore
+                saveFeedbackToFirestore(feedback);
+            }
+        });
+
+        return view;
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        String phoneNumberPattern = "\\d{3}-\\d{3}-\\d{4}";
+        return phoneNumber.matches(phoneNumberPattern);
+    }
+
+    private boolean isValidEmail(String email) {
+        // Use the built-in Patterns class to validate the email format
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void saveFeedbackToFirestore(Feedback feedback) {
+        db.collection("feedbacks")
+                .add(feedback)
+                .addOnSuccessListener(documentReference -> {
+                    // Feedback saved successfully
+                    Toast.makeText(getContext(), "Feedback submitted!", Toast.LENGTH_SHORT).show();
+
+                    // Reset the form
+                    editName.setText("");
+                    editPhone.setText("");
+                    editEmail.setText("");
+                    editComment.setText("");
+                    ratingBar.setRating(0);
+                })
+                .addOnFailureListener(e -> {
+                    // Failed to save feedback
+                    Toast.makeText(getContext(), "Failed to submit feedback. Please try again.", Toast.LENGTH_SHORT).show();
+                });
     }
 }
-
