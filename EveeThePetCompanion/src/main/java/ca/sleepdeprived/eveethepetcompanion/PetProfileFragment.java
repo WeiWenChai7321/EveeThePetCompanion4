de082c98ef231cd100bc3da30cc047f21cbfc77a
JohnAquino3112 <johnaquino460@gmail.com>
2023-07-15 13:49:20 -0400

    Added an onSaveInstanceState in PetProfileFragment.java as a base for saving a task the user has added on the calendar

--- EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/PetProfileFragment.java
+++ EveeThePetCompanion/src/main/java/ca/sleepdeprived/eveethepetcompanion/PetProfileFragment.java
@@ -257,4 +257,19 @@ public class PetProfileFragment extends Fragment {
         isEditMode = sharedPreferences.getBoolean("isEditMode", false);
         updateCellClickability();
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putBoolean("MyBoolean", true);
+        savedInstanceState.putDouble("myDouble", 1.9);
+        savedInstanceState.putInt("MyInt", 1);
+        savedInstanceState.putString("MyString", "Welcome back to Android");
+        // etc.
+    }
+
+
 }
